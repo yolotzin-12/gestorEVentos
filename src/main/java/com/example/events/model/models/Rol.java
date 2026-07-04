57e1fc48f19962769020709dceebe40e735e0d91
dxemely <20253ds093@utez.edu.mx>
2026-07-03 21:13:20 -0600

    agregando rol en models

---
+++ src/main/java/com/example/events/model/models/Rol.java
@@ -0,0 +1,42 @@
+package com.example.events.model.models;
+
+public class Rol {
+    private int id;
+    private String nombre;
+    private String descripcion;
+
+    public Rol(int id, String nombre, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return id + ',' + nombre + ',' + descripcion;
+    }
+}
