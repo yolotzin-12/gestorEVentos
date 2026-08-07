b2c7230ee4352ac7a4a7a2de245eb93f6f083c27
vanessa-mira <20253ds117@utez.edu.mx>
2026-08-07 00:41:49 -0600

    errores

--- src/main/java/com/example/events/model/models/Evento.java
+++ src/main/java/com/example/events/model/models/Evento.java
@@ -4,6 +4,7 @@ public class Evento {
     private int id;
     private int idOrganizador;
     private int idEspacio;
+    private int idCategoria; // <- Agregado
     private String nombre;
     private String nombreCategoria;
     private String imagenUrl;
@@ -14,45 +15,110 @@ public class Evento {
     private String fechaHora;
     private String estado; // "Borrador", "Disponible", "Cancelado"
 
-    public Evento() {}
+    public Evento() {
+    }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public int getId() {
+        return id;
+    }
 
-    public int getIdOrganizador() { return idOrganizador; }
-    public void setIdOrganizador(int idOrganizador) { this.idOrganizador = idOrganizador; }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-    public int getIdEspacio() { return idEspacio; }
-    public void setIdEspacio(int idEspacio) { this.idEspacio = idEspacio; }
+    public int getIdOrganizador() {
+        return idOrganizador;
+    }
 
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setIdOrganizador(int idOrganizador) {
+        this.idOrganizador = idOrganizador;
+    }
 
-    public String getNombreCategoria() { return nombreCategoria; }
-    public void setNombreCategoria(String nombreCategoria) { this.nombreCategoria = nombreCategoria; }
+    public int getIdEspacio() {
+        return idEspacio;
+    }
 
-    public String getDescripcion() { return descripcion; }
-    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
-
-    public int getCapacidadMaxima() { return capacidadMaxima; }
-    public void setCapacidadMaxima(int capacidadMaxima) { this.capacidadMaxima = capacidadMaxima; }
-
-    public int getCapacidadDisponible() { return capacidadDisponible; }
-    public void setCapacidadDisponible(int capacidadDisponible) { this.capacidadDisponible = capacidadDisponible; }
-
-    public String getUbicacion() { return ubicacion; }
-    public void setUbicacion(String ubicacion) { this.ubicacion = ubicacion; }
-
-    public String getFechaHora() { return fechaHora; }
-    public void setFechaHora(String fechaHora) { this.fechaHora = fechaHora; }
-
-    public String getEstado() { return estado; }
-    public void setEstado(String estado) { this.estado = estado; }
-
-    public String getImagenUrl() {return imagenUrl;}
-
-    public void setImagenUrl(String imagenUrl) {this.imagenUrl = imagenUrl;}
+    public void setIdEspacio(int idEspacio) {
+        this.idEspacio = idEspacio;
+    }
 
     public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombreCategoria() {
+        return nombreCategoria;
+    }
+
+    public void setNombreCategoria(String nombreCategoria) {
+        this.nombreCategoria = nombreCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public void setCapacidadMaxima(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+    public int getCapacidadDisponible() {
+        return capacidadDisponible;
+    }
+
+    public void setCapacidadDisponible(int capacidadDisponible) {
+        this.capacidadDisponible = capacidadDisponible;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(String fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getImagenUrl() {
+        return imagenUrl;
+    }
+
+    public void setImagenUrl(String imagenUrl) {
+        this.imagenUrl = imagenUrl;
     }
 }
