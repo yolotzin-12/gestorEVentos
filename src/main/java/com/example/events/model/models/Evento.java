dadc406566f24762e54be39ba753b9fcd93f5339
dxemely <20253ds093@utez.edu.mx>
2026-08-07 00:11:58 -0600

    cambios emely

--- src/main/java/com/example/events/model/models/Evento.java
+++ src/main/java/com/example/events/model/models/Evento.java
@@ -4,7 +4,6 @@ public class Evento {
     private int id;
     private int idOrganizador;
     private int idEspacio;
-    private int idCategoria;
     private String nombre;
     private String nombreCategoria;
     private String imagenUrl;
@@ -26,9 +25,6 @@ public class Evento {
 
     public int getIdEspacio() { return idEspacio; }
     public void setIdEspacio(int idEspacio) { this.idEspacio = idEspacio; }
-
-    public int getIdCategoria() { return idCategoria; }
-    public void setIdCategoria(int idCategoria) { this.idCategoria = idCategoria; }
 
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
